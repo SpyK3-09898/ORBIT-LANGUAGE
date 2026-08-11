80c5df83be1c1c20ee0b367212c0fc4dbc8fe5e4
SpyK3 <Unknow00op730@gmail.com>
2026-08-11 13:40:59 -0300

    VM ALPHA FIRST IMPL

--- _src/core/BackEnd/VM/virtual_machine.cpp
+++ _src/core/BackEnd/VM/virtual_machine.cpp
@@ -18,3 +18,44 @@
 #include <string>
 #include <variant>
 
+// ========== ENTRY-POINT | PONTO DE ENTRADA ========= //
+
+// Run A Instruction || Roda Uma Instrução.
+void VirtualMachine::Run()
+{
+    // MAIN LOOP | LOOP PRINCIPAL.
+    while (running) {
+    
+        // Data
+        VM_Frame& Frame = Calls.Top();
+        ByteInstruction* Inst =
+            Frame.C->Instructions[Frame.IP.Index];
+            
+        // Main Switch | Switch Principal.
+        switch(Inst->C)
+        {
+
+            default:
+                running=false;
+                        break;
+        }
+        Frame.IP.Index++;
+    }
+}
+
+// Entry-Point of Run Program || Ponto-De-Entrada do Programa de Execução.
+void VirtualMachine::InitVM(ByteCode& BC, RunTimeData& Data, Arena& Memory)
+{
+    // DATA
+    VM_Frame       Frame;
+    Code           = &BC;
+    Frame.C        = BC.Chunks[0];
+    Frame.IP.Index = 0;
+
+    // Set | Define.
+    Calls.Push(Frame);
+    running = true;
+
+    // Run Orbit | Roda A Orbit.
+    Run();
+}
--- _src/core/BackEnd/VM/virtual_machine.hpp
+++ _src/core/BackEnd/VM/virtual_machine.hpp
@@ -17,8 +17,83 @@
 #include "tools/console.hpp"
 #include "../../RunTimeData.hpp"
 
+// STRUCTS 
+
+// Bytecode Instruction-Pointer | Ponteiro do Bytecode.
+struct InstructionPointer
+{
+    ui32 Index;
+};
+
+// Frame Context of VM | Contexto do Quadro da VM.
+struct VM_Frame
+{
+    Chunk* C;
+    InstructionPointer IP;
+    vec<ByteValue> Locals;
+};
+
+// Context of Calls | Contexto de Chamada.
+struct CallStack
+{
+    // DATA:
+    vec<VM_Frame> Frames;
+
+    // UTILS | UTILIDADES:
+
+    // Return Top of Frames | Retorna o Topo dos Frames.
+    VM_Frame& Top()
+    {
+        return Frames.back();
+    }
+
+    // Add a New Frame | Adiciona Um Novo Frame.
+    void Push(VM_Frame Frame)
+    {
+        Frames.push_back(Frame);
+    }
+
+    // Remove A Frame | Remove um Frame.
+    void Pop()
+    {
+        Frames.pop_back();
+    }
+};
+
+// Stack of VM | Pilha da VM.
+struct Stack
+{
+    // DATA
+    vec<ByteValue> Data;
+    int Top;
+
+    // UTILS | UTILIDADES
+    // Remove 1 Value of Stack | Remove Um Valor na Stack.
+    ByteValue Pop()
+    { 
+        ByteValue V = Data.back();
+        Data.pop_back(); 
+        Top = Data.empty() ? -1 : Data.size() - 1;
+        return V;
+    }
+    void Push(ByteValue Val)
+    {
+        Data.push_back(Val);
+        Top = Data.size() - 1;
+    }
+};
+
 // MAIN CLASS | CLASSE PRINCIPAL.
 class VirtualMachine
 {
+    private:
     
+        ByteCode* Code;
+        Stack Stack;
+        CallStack Calls;
+        bool running=false;
+    public:
+
+        void Run();
+        void InitVM(ByteCode& BC, RunTimeData& Data, Arena& Memory);
 };
